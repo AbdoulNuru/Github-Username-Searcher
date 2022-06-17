1c0044428bbf93de7f8f6ae1a35e3eb0cf4b42b3
AbdoulNuru <abdoulniyigena@gmail.com>
2022-06-17 23:57:35 +0200

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Info from "./Info";
-import Repos from "./Repositories";
-import User from "./User";
-import Search from "./Search";
-import Navbar from "./Navbar";
-
-export { Info, Repos, User, Search, Navbar };
