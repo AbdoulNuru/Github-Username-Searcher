b536d77852112aab374858807d6bb8ca5dca6022
AbdoulNuru <abdoulniyigena@gmail.com>
2022-06-17 15:05:46 +0200

    bg: fix issues consumed endpoints

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [username, setUsername] = useState('');
+  const { searchUser } = useContext(GithubContext);
 
   const onSubmit = (e) => {
     e.preventDefault();
     
+    if(username){
+      searchUser(username);
+      // setUsername('');
+    }
   }
 
   return (
--- src/context/context.js
+++ src/context/context.js
@@ -4,6 +4,7 @@ import mockUser from './mockData.js/userMockData';
 import mockRepos from './mockData.js/repoMockData';
 import mockFollowers from './mockData.js/followersMockData';
 import mockFollowing from './mockData.js/followingMockData';
+import followers from './mockData.js/followersMockData';
 
 
 const url = 'https://api.github.com';
@@ -18,9 +19,38 @@ const GithubProvider = ({ children }) => {
     [loading, setLoading] = useState(false),
     [requests, setRequests] = useState(0);
 
+    const searchUser = async (user) => {
+      const response = await axios
+        .get(`${url}/users/${user}`)
+        .catch((err) => console.log(err));
+  
+      console.log(response);
+      if (response) {
+        setGitUser(response.data);
+        console.log(response);
+        const { followers_url, login } = response.data;
+  
+        await Promise.allSettled([
+          axios.get(`${url}/users/${login}/repos`),
+          axios.get(`${url}/${followers_url}?per_page=100`),
+        ])
+          .then((results) => {
+            const [repos, followers] = results;
+            const fulfilledStatus = 'fulfilled';
+  
+            if (repos.status === fulfilledStatus) {
+              setGitRepos(repos.value.data);
+            }
+            if (followers.status === fulfilledStatus) {
+              setGitFollowers(followers.value.data);
+            }
+          })
+          .catch((err) => console.log(err));
+      } 
+    };
 
   return (
-    <GithubContext.Provider value={{gitUser, gitRepos, gitFollowers, gitFollowing}}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{gitUser, gitRepos, gitFollowers, gitFollowing, searchUser}}>{children}</GithubContext.Provider>
   );
 };
 
